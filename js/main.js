1d7b7142fdd037b09716deeb2c6b78db5fec5cec
Meylis Gazakow <Meylis7@users.noreply.github.com>
2022-10-22 13:50:54 +0500

    new Home page done

--- js/main.js
+++ js/main.js
@@ -44,6 +44,12 @@ let burger = selectElement('.hamburg');
 let langMobile = selectElement('.langMobile');
 let languagesMobile = selectElement('.languagesMobile');
 
+
+let video_item = document.querySelectorAll('.media_video-item');
+let modal_video = document.querySelector('.modal_video');
+let body = document.querySelector('body');
+
+
 // console.log(burger);
 
 window.onclick = function (e) {
@@ -61,16 +67,23 @@ window.onclick = function (e) {
     if (selectElement('.navs__inner').classList.contains('open') && !e.target.closest('.open__link') && !e.target.closest('.nav__group')) {
         selectElement('.navs__inner').classList.remove('open');
     }
-    
+
     if (selectElement('.hidden__media').classList.contains('open')) {
         selectElement('.hidden__media').classList.remove('open');
     }
-    
 
-    if (selectElement('.player_bg').classList.contains('videoShow') && !e.target.closest('.player')) {
-        selectElement('.player_bg').classList.remove('videoShow');
-        selectElement('.player__video').pause();
-        selectElement('.player__video').currentTime = 0;
+
+    // if (selectElement('.player_bg').classList.contains('videoShow') && !e.target.closest('.player')) {
+    //     selectElement('.player_bg').classList.remove('videoShow');
+    //     selectElement('.player__video').pause();
+    //     selectElement('.player__video').currentTime = 0;
+    // }
+
+    if (modal_video != undefined) {
+        if (modal_video.classList.contains('active') && !e.target.closest('.modal_video-item video')) {
+            modal_video.classList.remove('active')
+            body.classList.remove('active')
+        }
     }
 
 }
@@ -79,6 +92,19 @@ function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+// =============================================
+
+if (video_item != undefined) {
+    video_item.forEach(x => {
+        x.addEventListener('click', function () {
+            sleep(2).then(() => {
+                modal_video.classList.add('active');
+                body.classList.add('active');
+                console.log("log")
+            });
+        });
+    })
+}
 
 
 
@@ -103,11 +129,11 @@ selectElement('.new__hamburg').addEventListener('click', function () {
     });
 });
 
-selectElement('.nav_open_media').addEventListener('click', function () {
-    sleep(2).then(() => {
-        selectElement('.hidden__media').classList.toggle('open');
-    });
-});
+// selectElement('.nav_open_media').addEventListener('click', function () {
+//     sleep(2).then(() => {
+//         selectElement('.hidden__media').classList.toggle('open');
+//     });
+// });
 
 selectElement('.mobile__close').addEventListener('click', function () {
     sleep(2).then(() => {
@@ -138,108 +164,105 @@ document.querySelectorAll('.gallery_video').forEach((e) => {
     })
 });
 
-document.querySelectorAll('.video_item').forEach((e) => {
-    e.addEventListener('click', function () {
-        sleep(2).then(() => {
-            selectElement('.player_bg').classList.toggle('videoShow');
-            var vd = selectElement('.player__video');
-            while (vd.lastElementChild) {
-                vd.removeChild(vd.lastElementChild);
-            }
+// document.querySelectorAll('.video_item').forEach((e) => {
+//     e.addEventListener('click', function () {
+//         sleep(2).then(() => {
+//             selectElement('.player_bg').classList.toggle('videoShow');
+//             var vd = selectElement('.player__video');
+//             while (vd.lastElementChild) {
+//                 vd.removeChild(vd.lastElementChild);
+//             }
 
-            var source = document.createElement('source');
-            console.log(e)
-            console.log(e.dataset.src)
+//             var source = document.createElement('source');
+//             // console.log(e)
+//             // console.log(e.dataset.src)
 
-            source.setAttribute('src', e.dataset.src);
+//             source.setAttribute('src', e.dataset.src);
 
-            vd.appendChild(source);
-            vd.load();
-            vd.play();
-        });
-    })
-});
-// }   
+//             vd.appendChild(source);
+//             vd.load();
+//             vd.play();
+//         });
+//     })
+// });
 
-document.querySelectorAll('.player__video').forEach((e) => {
-    e.addEventListener('ended', function () {
-        // sleep(0).then(() => {
-        selectElement('.player_bg').classList.remove('videoShow');
-        // });
-    })
-});
+// document.querySelectorAll('.player__video').forEach((e) => {
+//     e.addEventListener('ended', function () {
+//         selectElement('.player_bg').classList.remove('videoShow');
+//     })
+// });
 
 // // End Header
 
-// Video Player 
-const video = document.querySelector('.viewer');
-const toggle = document.querySelector('.toggle');
-const volume = document.querySelector('.volume');
-const skipButtons = document.querySelectorAll('[data-skip]');
-const ranges = document.querySelectorAll('.player__slider');
-const progress = document.querySelector('.progress');
-const progressBar = document.querySelector('.progress__filled');
-const fullscreen = document.querySelector('.fullscreen');
+// Video Player
+// const video = document.querySelector('.viewer');
+// const toggle = document.querySelector('.toggle');
+// const volume = document.querySelector('.volume');
+// const skipButtons = document.querySelectorAll('[data-skip]');
+// const ranges = document.querySelectorAll('.player__slider');
+// const progress = document.querySelector('.progress');
+// const progressBar = document.querySelector('.progress__filled');
+// const fullscreen = document.querySelector('.fullscreen');
 
 
-// defining functions
-function togglePlay() {
-    // toggle for video play and pause
-    const playOrPause = video.paused ? 'play' : 'pause';
-    video[playOrPause]();
-    // toggle for icon change when play or pause
-    playOrPause === 'play' ? toggle.textContent = '❚ ❚' : toggle.textContent = '►';
-}
+// // defining functions
+// function togglePlay() {
+//     // toggle for video play and pause
+//     const playOrPause = video.paused ? 'play' : 'pause';
+//     video[playOrPause]();
+//     // toggle for icon change when play or pause
+//     playOrPause === 'play' ? toggle.textContent = '❚ ❚' : toggle.textContent = '►';
+// }
 
-function skip() {
-    // add or substract the skip time to current time of video
-    video.currentTime += parseFloat(this.dataset.skip);
-}
+// function skip() {
+//     // add or substract the skip time to current time of video
+//     video.currentTime += parseFloat(this.dataset.skip);
+// }
 
-function handleRangeChange() {
-    // Change the video's range value
-    video[this.name] = this.value;
-}
+// function handleRangeChange() {
+//     // Change the video's range value
+//     video[this.name] = this.value;
+// }
 
-function handleProgress() {
-    // convert video's current time into percentage
-    const percent = (video.currentTime / video.duration) * 100;
-    // append it to the flexBasis property (CSS)
-    progressBar.style.flexBasis = `${percent}%`;
-}
+// function handleProgress() {
+//     // convert video's current time into percentage
+//     const percent = (video.currentTime / video.duration) * 100;
+//     // append it to the flexBasis property (CSS)
+//     progressBar.style.flexBasis = `${percent}%`;
+// }
 
-function scrub(e) {
-    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-    video.currentTime = scrubTime;
-}
+// function scrub(e) {
+//     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+//     video.currentTime = scrubTime;
+// }
 
-function handleFullScreen() {
-    video.requestFullscreen();
-}
+// function handleFullScreen() {
+//     video.requestFullscreen();
+// }
 
-// Play or Pause events(On video click)
-video.addEventListener('click', togglePlay);
-// 					   (On button click)
-toggle.addEventListener('click', togglePlay);
+// // Play or Pause events(On video click)
+// video.addEventListener('click', togglePlay);
+// // 					   (On button click)
+// toggle.addEventListener('click', togglePlay);
 
-// skipping video back and forth
-skipButtons.forEach(button => button.addEventListener('click', skip));
+// // skipping video back and forth
+// skipButtons.forEach(button => button.addEventListener('click', skip));
 
-// volume or fast forward events
-ranges.forEach(range => range.addEventListener('change', handleRangeChange));
+// // volume or fast forward events
+// ranges.forEach(range => range.addEventListener('change', handleRangeChange));
 
-// Change progress wrt time
-video.addEventListener('timeupdate', handleProgress);
+// // Change progress wrt time
+// video.addEventListener('timeupdate', handleProgress);
 
-// event on clicking progress bar
-let mouseDown = false;
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
-progress.addEventListener('mousedown', () => mouseDown = true);
-progress.addEventListener('mouseup', () => mouseDown = false);
+// // event on clicking progress bar
+// let mouseDown = false;
+// progress.addEventListener('click', scrub);
+// progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
+// progress.addEventListener('mousedown', () => mouseDown = true);
+// progress.addEventListener('mouseup', () => mouseDown = false);
 
-// add full screen event
-fullscreen.addEventListener('click', handleFullScreen);
+// // add full screen event
+// fullscreen.addEventListener('click', handleFullScreen);
 
 
 
